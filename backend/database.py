a0e221eae829681e93dae34737fba15941473976
Sandesh Kuikel <107356018+sandeshkuikel07@users.noreply.github.com>
2025-05-15 20:32:55 +0545

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -4,9 +4,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from backend.config import settings
 
 engine = create_engine(
-    DATABASE_URL: str = "postgresql://postgres:password@localhost:5432/test_db"
-    settings.DATABASE_URL,
-    connect_args={"check_same_thread": False}  # for SQLite
+    settings.DATABASE_URL
+    # connect_args={"check_same_thread": False}  # for SQLite
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

84c2c3a307869e770a75fd8565a64e2381019e97
Sandesh Kuikel <107356018+sandeshkuikel07@users.noreply.github.com>
2025-05-15 20:07:21 +0545

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     ENVIRONMENT: str = "dev"
     
     # Database
-    DATABASE_URL: str = "sqlite:///./taskflow.db"
+    DATABASE_URL: str = "postgresql://postgres:password@localhost:5432/test_db"
     
     # Security
     SECRET_KEY: str = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
